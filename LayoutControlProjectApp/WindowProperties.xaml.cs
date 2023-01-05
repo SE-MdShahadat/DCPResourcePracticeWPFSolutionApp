8bc994cf92d04302dff8918eca710124097f468b
SE-MdShahadat <se.mdshahadat@gmail.com>
2023-01-05 15:44:35 +0600

    Chapter 3 Layout Control

---
+++ LayoutControlProjectApp/WindowProperties.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LayoutControlProjectApp
+{
+    /// <summary>
+    /// Interaction logic for WindowProperties.xaml
+    /// </summary>
+    public partial class WindowProperties : Window
+    {
+        public WindowProperties()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            b1.Content = "this button is clicked";
+        }
+    }
+}
